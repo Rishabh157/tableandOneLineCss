1bfcdb81cbc72fa29b3b2859ba9532aeaa6c4176
rishabh157 <rishabhgour157@gmail.com>
2023-01-14 22:15:28 +0530

    added 8 title for css events oneline code

--- src/ONELINECSS/OneLineCSS.jsx
+++ src/ONELINECSS/OneLineCSS.jsx
@@ -60,6 +60,69 @@ export default function OneLineCSS() {
 
             {/* for scrolling snap scroll end */}
 
+
+
+            {/* this is for truncate start */}
+
+            <div className="d-flex justify-center">
+                <div className="p-3 bg-lime-300" style={{ width: "400px" }}>
+                    <div><h1>HELLO Truncate</h1></div>
+                    <div className="truncate-class">
+                        <p>
+                            Lorem Ipsum is simply dummy text of the printing
+                            and typesetting industry. Lorem Ipsum has been
+                            the industry's standard dummy text ever since the
+                            1500s, when an unknown printer took a galley of type
+                            and scrambled it to make a type specimen book. It
+                            has survived not only five centuries, but also the
+                            leap into electronic typesetting, remaining essentially
+                            unchanged. It was popularised in the 1960s with the release
+                            of Letraset sheets containing Lorem Ipsum passages, and
+                            more recently with desktop publishing software like Aldus PageMaker
+                            including versions of Lorem Ipsum
+                        </p>
+                    </div>
+                </div>
+            </div>
+
+            {/* this is for truncate end */}
+
+            {/* for text color gradient start */}
+            <div className="flex justify-center my-24">
+                <div>
+                    <p>before taking a gradient class</p>
+
+                    <h1 className="text-9xl tracking-wide font-bold before-gradient">
+                        APPLE
+                    </h1>
+                </div>
+            </div>
+
+
+            <div className="flex justify-center my-24">
+                <div>
+                    <p>after taking a gradient class</p>
+                    <h1 className="text-9xl tracking-wide font-bold after-gradient">
+                        APPLE
+                    </h1>
+                </div>
+            </div>
+            {/* for text color gradient end */}
+
+
+            {/* this is set the actual image we don't need to define the width and height for image  */}
+
+            <div className="objectFitclass">
+                <img src="https://images.unsplash.com/photo-1673494735754-196c4d8ededd?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=870&q=80" ></img>
+            </div>
+
+            {/* this is set the actual image we don't need to define the width and height for image  */}
+
+
+            {/* poiner event  */}
+            <h1 className="text-9xl tracking-wide pointer_event">
+                POINTER EVENT
+            </h1>
         </div>
     )
 }
